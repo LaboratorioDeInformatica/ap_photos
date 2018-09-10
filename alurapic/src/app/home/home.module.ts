8896c0781d1c1c1d6add15b9dbe1bfc474e1aded
unknown <labdeinf@gmail.com>
2018-09-09 21:57:06 -0300

    modularização da mensagem de valodação

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule} from '@angular/forms'
 import { CommonModule } from '@angular/common';
+import { VMessageComponent } from '../shared/components/vmessage/vmessage.componet';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations:[SigninComponent],
     imports:[
             ReactiveFormsModule,
-            CommonModule]
+            CommonModule,
+            VMessageModule
+    ]
 })
 export class HomeModule{
     
---
+++ alurapic/src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { VMessageComponent } from './vmessage.componet';
+
+@NgModule({
+    declarations:[VMessageComponent],
+    exports:[VMessageComponent]
+})
+export class VMessageModule{}
