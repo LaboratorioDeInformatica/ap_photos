ca7d978082134a8d757510b2f386651d303f2696
unknown <labdeinf@gmail.com>
2018-09-16 16:58:18 -0300

    BehaviorSubject

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{}
+export class HeaderComponent{
+
+
+    user$: Observable<User>;
+    user: User;
+
+    constructor (private userService: UserService){
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);   
+    }
+}
